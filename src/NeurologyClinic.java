cbcb798b57b4e5de587260390871638695ed6503
jjaroslaw <jar3q1@o2.pl>
2018-04-20 17:05:04 +0200

    update

--- src/NeurologyClinic.java
+++ src/NeurologyClinic.java
@@ -14,25 +14,50 @@ public class NeurologyClinic extends Clinic{
 	Clinic clinic = new Clinic();
 	
 	JFrame frame = new JFrame("Neurology Clinic");
+	
 	JPanel panel = new JPanel();
 	JPanel mainPanel = new JPanel();
 	JPanel doctorPanel = new JPanel();
+	JPanel patientPanel = new JPanel();
+	
 	JButton addDoctorButton = new JButton("Add doctor to Database");
 	JButton addDoctor = new JButton("Add doctor");
 	JButton addPatientButton = new JButton("Add patient to Database");
+	JButton addPatient = new JButton("Add patient");
 	JButton addHealthStaffButton = new JButton("Add health staff to Database");
 	
+	JLabel addDoctorTitle;
+	JLabel addName;
 	JTextField nameField;
+	JLabel addSurname;
 	JTextField surnameField;
+	JLabel addPersonalNumber;
 	JTextField personalNumberField;
+	JLabel addDateOfBirth;
 	JTextField dateOfBirthField;
+	JLabel addCountryOfBirth;
+	JComboBox countryOfBirthCB;
+	JLabel addCity;
 	JTextField cityField;
+	JLabel addPostalCode;
 	JTextField postalCodeField;
+	JLabel addStreetAndHouseNumber;
 	JTextField streetAndHouseNumberField;
+	JLabel addPhone;
 	JTextField phoneField;
+	JLabel addSex;
+	JComboBox sexCB;
+	JLabel addLicenceNumber;
 	JTextField licenceNumberField;
+	JLabel addSpecialty;
+	JComboBox specialtyCB;
+	JLabel addDateOfEmployment;
 	JTextField dateOfEmploymentField;
 	JLabel wrongData;
+	
+	JLabel addPatientTitle;
+	JLabel addDateOfRegistration;
+	JTextField dateOfRegistrationField;
 	
 	
 	JButton mainMenuButton = new JButton("Main Menu");
@@ -44,14 +69,116 @@ public class NeurologyClinic extends Clinic{
 		
 		panel.add(mainPanel, "Main Panel");
 		panel.add(doctorPanel, "Adding Doctor Panel");
+		panel.add(patientPanel, "Adding Patient Panel");
 		cl.show(panel, "Main Panel");
 		
 		addDoctorButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				cl.show(panel, "Adding Doctor Panel");
-				System.out.println("Add doctor");
+				GroupLayout doctorPanelLayout = new GroupLayout(doctorPanel);
+				doctorPanel.setLayout(doctorPanelLayout);
+				doctorPanelLayout.setAutoCreateGaps(true);
+				doctorPanelLayout.setAutoCreateContainerGaps(true);
+				doctorPanel.setBackground(new Color(228, 228, 228));
+				
+				doctorPanelLayout.setHorizontalGroup(doctorPanelLayout.createParallelGroup()
+						.addComponent(addDoctorTitle)
+						.addGap(30, 30, Short.MAX_VALUE)
+						.addGroup(doctorPanelLayout.createParallelGroup()
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+									.addComponent(addName, 10, 10, Short.MAX_VALUE).addComponent(nameField, 10, 10, Short.MAX_VALUE).addComponent(addSurname, 10, 10, Short.MAX_VALUE).addComponent(surnameField, 10, 10, Short.MAX_VALUE))
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+									.addComponent(addPersonalNumber, 10, 10, Short.MAX_VALUE).addComponent(personalNumberField, 10, 10, Short.MAX_VALUE).addComponent(addDateOfBirth, 10, 10, Short.MAX_VALUE).addComponent(dateOfBirthField, 10, 10, Short.MAX_VALUE))
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+									.addComponent(addCountryOfBirth, 10, 10, Short.MAX_VALUE).addComponent(countryOfBirthCB, 10, 10, Short.MAX_VALUE).addComponent(addCity, 10, 10, Short.MAX_VALUE).addComponent(cityField, 10, 10, Short.MAX_VALUE))
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+										.addComponent(addPostalCode, 10, 10, Short.MAX_VALUE).addComponent(postalCodeField, 10, 10, Short.MAX_VALUE).addComponent(addStreetAndHouseNumber, 10, 10, Short.MAX_VALUE).addComponent(streetAndHouseNumberField, 10, 10, Short.MAX_VALUE))
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+										.addComponent(addPhone, 10, 10, Short.MAX_VALUE).addComponent(phoneField, 10, 10, Short.MAX_VALUE).addComponent(addSex, 10, 10, Short.MAX_VALUE).addComponent(sexCB, 10, 10, Short.MAX_VALUE))
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+										.addComponent(addLicenceNumber, 10, 10, Short.MAX_VALUE).addComponent(licenceNumberField, 10, 10, Short.MAX_VALUE).addComponent(addSpecialty, 10, 10, Short.MAX_VALUE).addComponent(specialtyCB, 10, 10, Short.MAX_VALUE)))
+						.addGroup(doctorPanelLayout.createSequentialGroup()
+								.addComponent(addDateOfEmployment, 10, 10, Short.MAX_VALUE).addGap(10,10,10).addComponent(dateOfEmploymentField, 10, 10, Short.MAX_VALUE).addGap(247, 247, Short.MAX_VALUE))
+						.addGap(30, 30, Short.MAX_VALUE)
+						.addGroup(doctorPanelLayout.createSequentialGroup()
+								.addComponent(wrongData).addGap(100, 100, Short.MAX_VALUE).addComponent(addDoctor).addComponent(mainMenuButton))
+						);
+				
+				doctorPanelLayout.setVerticalGroup(doctorPanelLayout.createSequentialGroup()
+						.addComponent(addDoctorTitle)
+						.addGap(30, 30, Short.MAX_VALUE)
+						.addGroup(doctorPanelLayout.createParallelGroup()
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+									.addComponent(addName, 20, 20, Short.MAX_VALUE).addComponent(addPersonalNumber, 20, 20, Short.MAX_VALUE).addComponent(addCountryOfBirth, 20, 20, Short.MAX_VALUE).addComponent(addPostalCode, 20, 20, Short.MAX_VALUE).addComponent(addPhone, 20, 20, Short.MAX_VALUE).addComponent(addLicenceNumber, 20, 20, Short.MAX_VALUE))
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+									.addComponent(nameField, 20, 20, Short.MAX_VALUE).addComponent(personalNumberField, 20, 20, Short.MAX_VALUE).addComponent(countryOfBirthCB, 20, 20, Short.MAX_VALUE).addComponent(postalCodeField, 20, 20, Short.MAX_VALUE).addComponent(phoneField, 20, 20, Short.MAX_VALUE).addComponent(licenceNumberField, 20, 20, Short.MAX_VALUE))
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+										.addComponent(addSurname, 20, 20, Short.MAX_VALUE).addComponent(addDateOfBirth, 20, 20, Short.MAX_VALUE).addComponent(addCity, 20, 20, Short.MAX_VALUE).addComponent(addStreetAndHouseNumber, 20, 20, Short.MAX_VALUE).addComponent(addSex, 20, 20, Short.MAX_VALUE).addComponent(addSpecialty, 20, 20, Short.MAX_VALUE))
+								.addGroup(doctorPanelLayout.createSequentialGroup()
+										.addComponent(surnameField, 20, 20, Short.MAX_VALUE).addComponent(dateOfBirthField, 20, 20, Short.MAX_VALUE).addComponent(cityField, 20, 20, Short.MAX_VALUE).addComponent(streetAndHouseNumberField, 20, 20, Short.MAX_VALUE).addComponent(sexCB, 20, 20, Short.MAX_VALUE).addComponent(specialtyCB, 20, 20, Short.MAX_VALUE)))
+						.addGroup(doctorPanelLayout.createParallelGroup()
+								.addComponent(addDateOfEmployment, 20, 20, 23).addComponent(dateOfEmploymentField, 10, 10, 23))
+						.addGap(30, 30, Short.MAX_VALUE)
+						.addGroup(doctorPanelLayout.createParallelGroup()
+								.addComponent(wrongData).addComponent(addDoctor).addComponent(mainMenuButton))
+						);
 			}
+		});
+		
+		addPatientButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cl.show(panel, "Adding Patient Panel");
+				
+				GroupLayout patientPanelLayout = new GroupLayout(patientPanel);
+				patientPanel.setLayout(patientPanelLayout);
+				patientPanelLayout.setAutoCreateGaps(true);
+				patientPanelLayout.setAutoCreateContainerGaps(true);
+				patientPanel.setBackground(new Color(228, 228, 228));
+				
+				patientPanelLayout.setHorizontalGroup(patientPanelLayout.createParallelGroup()
+						.addComponent(addPatientTitle)//
+						.addGap(30, 30, Short.MAX_VALUE)
+						.addGroup(patientPanelLayout.createParallelGroup()
+								.addGroup(patientPanelLayout.createSequentialGroup()
+									.addComponent(addName, 10, 10, Short.MAX_VALUE).addComponent(nameField, 10, 10, Short.MAX_VALUE).addComponent(addSurname, 10, 10, Short.MAX_VALUE).addComponent(surnameField, 10, 10, Short.MAX_VALUE))
+								.addGroup(patientPanelLayout.createSequentialGroup()
+									.addComponent(addPersonalNumber, 10, 10, Short.MAX_VALUE).addComponent(personalNumberField, 10, 10, Short.MAX_VALUE).addComponent(addDateOfBirth, 10, 10, Short.MAX_VALUE).addComponent(dateOfBirthField, 10, 10, Short.MAX_VALUE))
+								.addGroup(patientPanelLayout.createSequentialGroup()
+									.addComponent(addCountryOfBirth, 10, 10, Short.MAX_VALUE).addComponent(countryOfBirthCB, 10, 10, Short.MAX_VALUE).addComponent(addCity, 10, 10, Short.MAX_VALUE).addComponent(cityField, 10, 10, Short.MAX_VALUE))
+								.addGroup(patientPanelLayout.createSequentialGroup()
+										.addComponent(addPostalCode, 10, 10, Short.MAX_VALUE).addComponent(postalCodeField, 10, 10, Short.MAX_VALUE).addComponent(addStreetAndHouseNumber, 10, 10, Short.MAX_VALUE).addComponent(streetAndHouseNumberField, 10, 10, Short.MAX_VALUE))
+								.addGroup(patientPanelLayout.createSequentialGroup()
+										.addComponent(addPhone, 10, 10, Short.MAX_VALUE).addComponent(phoneField, 10, 10, Short.MAX_VALUE).addComponent(addSex, 10, 10, Short.MAX_VALUE).addComponent(sexCB, 10, 10, Short.MAX_VALUE)))
+						.addGroup(patientPanelLayout.createSequentialGroup()
+								.addComponent(addDateOfRegistration, 10, 10, Short.MAX_VALUE).addGap(10,10,10).addComponent(dateOfRegistrationField, 10, 10, Short.MAX_VALUE).addGap(247, 247, Short.MAX_VALUE))
+						.addGap(30, 30, Short.MAX_VALUE)
+						.addGroup(patientPanelLayout.createSequentialGroup()
+								.addComponent(wrongData).addGap(100, 100, Short.MAX_VALUE).addComponent(addPatient).addComponent(mainMenuButton))
+						);
+				
+				patientPanelLayout.setVerticalGroup(patientPanelLayout.createSequentialGroup()
+						.addComponent(addPatientTitle)
+						.addGap(30, 30, Short.MAX_VALUE)
+						.addGroup(patientPanelLayout.createParallelGroup()
+								.addGroup(patientPanelLayout.createSequentialGroup()
+									.addComponent(addName, 20, 20, Short.MAX_VALUE).addComponent(addPersonalNumber, 20, 20, Short.MAX_VALUE).addComponent(addCountryOfBirth, 20, 20, Short.MAX_VALUE).addComponent(addPostalCode, 20, 20, Short.MAX_VALUE).addComponent(addPhone, 20, 20, Short.MAX_VALUE))
+								.addGroup(patientPanelLayout.createSequentialGroup()
+									.addComponent(nameField, 20, 20, Short.MAX_VALUE).addComponent(personalNumberField, 20, 20, Short.MAX_VALUE).addComponent(countryOfBirthCB, 20, 20, Short.MAX_VALUE).addComponent(postalCodeField, 20, 20, Short.MAX_VALUE).addComponent(phoneField, 20, 20, Short.MAX_VALUE))
+								.addGroup(patientPanelLayout.createSequentialGroup()
+										.addComponent(addSurname, 20, 20, Short.MAX_VALUE).addComponent(addDateOfBirth, 20, 20, Short.MAX_VALUE).addComponent(addCity, 20, 20, Short.MAX_VALUE).addComponent(addStreetAndHouseNumber, 20, 20, Short.MAX_VALUE).addComponent(addSex, 20, 20, Short.MAX_VALUE))
+								.addGroup(patientPanelLayout.createSequentialGroup()
+										.addComponent(surnameField, 20, 20, Short.MAX_VALUE).addComponent(dateOfBirthField, 20, 20, Short.MAX_VALUE).addComponent(cityField, 20, 20, Short.MAX_VALUE).addComponent(streetAndHouseNumberField, 20, 20, Short.MAX_VALUE).addComponent(sexCB, 20, 20, Short.MAX_VALUE)))
+						.addGroup(patientPanelLayout.createParallelGroup()
+								.addComponent(addDateOfRegistration, 20, 20, 23).addComponent(dateOfRegistrationField, 10, 10, 23))
+						.addGap(30, 30, Short.MAX_VALUE)
+						.addGroup(patientPanelLayout.createParallelGroup()
+								.addComponent(wrongData).addComponent(addPatient).addComponent(mainMenuButton))
+						);
+			}
+			
 		});
 		
 		mainMenuButton.addActionListener(new ActionListener() {
@@ -81,34 +208,34 @@ public class NeurologyClinic extends Clinic{
 				.addComponent(addHealthStaffButton)
 				);
 		
-		//Add doctor menu
-		JLabel addDoctorTitle = new JLabel("Add doctor");
+		// Doctor Panel
+		addDoctorTitle = new JLabel("Add doctor");
 		addDoctorTitle.setFont(new Font("Arial", Font.BOLD, 37));
-		JLabel addName = new JLabel("Name:");
+		addName = new JLabel("Name:");
 		nameField = new JTextField();
-		JLabel addSurname = new JLabel("Surname:");
+		addSurname = new JLabel("Surname:");
 		surnameField = new JTextField();
-		JLabel addPersonalNumber = new JLabel("Personal Number:");
+		addPersonalNumber = new JLabel("Personal Number:");
 		personalNumberField = new JTextField();
-		JLabel addDateOfBirth = new JLabel("Date of birth (yyyy-mm-dd):");
+		addDateOfBirth = new JLabel("Date of birth (yyyy-mm-dd):");
 		dateOfBirthField = new JTextField();
-		JLabel addCountryOfBirth = new JLabel("Country of birth:");
-		JComboBox countryOfBirthCB = new JComboBox(clinic.selectCountries().toArray());
-		JLabel addCity = new JLabel("City:");
+		addCountryOfBirth = new JLabel("Country of birth:");
+		countryOfBirthCB = new JComboBox(clinic.selectCountries().toArray());
+		addCity = new JLabel("City:");
 		cityField = new JTextField();
-		JLabel addPostalCode = new JLabel("Postal code (xx-xxx):");
+		addPostalCode = new JLabel("Postal code (xx-xxx):");
 		postalCodeField = new JTextField();
-		JLabel addStreetAndHouseNumber = new JLabel("Street and house number:");
+		addStreetAndHouseNumber = new JLabel("Street and house number:");
 		streetAndHouseNumberField = new JTextField();
-		JLabel addPhone = new JLabel("Phone number:");
+		addPhone = new JLabel("Phone number:");
 		phoneField = new JTextField();
-		JLabel addSex = new JLabel("Sex:");
-		JComboBox sexCB = new JComboBox(clinic.selectSex().toArray());
-		JLabel addLicenceNumber = new JLabel("Licence number:");
+		addSex = new JLabel("Sex:");
+		sexCB = new JComboBox(clinic.selectSex().toArray());
+		addLicenceNumber = new JLabel("Licence number:");
 		licenceNumberField = new JTextField();
-		JLabel addSpecialty = new JLabel("Specialty:");
-		JComboBox specialtyCB = new JComboBox(clinic.selectSpecialty().toArray());
-		JLabel addDateOfEmployment = new JLabel("Date of employment (yyyy-mm-dd):");
+		addSpecialty = new JLabel("Specialty:");
+		specialtyCB = new JComboBox(clinic.selectSpecialty().toArray());
+		addDateOfEmployment = new JLabel("Date of employment (yyyy-mm-dd):");
 		dateOfEmploymentField = new JTextField();
 		
 		wrongData = new JLabel("Wrong data! Check everything.");
@@ -116,53 +243,6 @@ public class NeurologyClinic extends Clinic{
 		wrongData.setVisible(false);
 		
 		
-		GroupLayout doctorPanelLayout = new GroupLayout(doctorPanel);
-		doctorPanel.setLayout(doctorPanelLayout);
-		doctorPanelLayout.setAutoCreateGaps(true);
-		doctorPanelLayout.setAutoCreateContainerGaps(true);
-		doctorPanel.setBackground(new Color(228, 228, 228));
-		
-		doctorPanelLayout.setHorizontalGroup(doctorPanelLayout.createParallelGroup()
-				.addComponent(addDoctorTitle)
-				.addGap(30, 30, Short.MAX_VALUE)
-				.addGroup(doctorPanelLayout.createParallelGroup()
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-							.addComponent(addName, 10, 10, Short.MAX_VALUE).addComponent(nameField, 10, 10, Short.MAX_VALUE).addComponent(addSurname, 10, 10, Short.MAX_VALUE).addComponent(surnameField, 10, 10, Short.MAX_VALUE))
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-							.addComponent(addPersonalNumber, 10, 10, Short.MAX_VALUE).addComponent(personalNumberField, 10, 10, Short.MAX_VALUE).addComponent(addDateOfBirth, 10, 10, Short.MAX_VALUE).addComponent(dateOfBirthField, 10, 10, Short.MAX_VALUE))
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-							.addComponent(addCountryOfBirth, 10, 10, Short.MAX_VALUE).addComponent(countryOfBirthCB, 10, 10, Short.MAX_VALUE).addComponent(addCity, 10, 10, Short.MAX_VALUE).addComponent(cityField, 10, 10, Short.MAX_VALUE))
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-								.addComponent(addPostalCode, 10, 10, Short.MAX_VALUE).addComponent(postalCodeField, 10, 10, Short.MAX_VALUE).addComponent(addStreetAndHouseNumber, 10, 10, Short.MAX_VALUE).addComponent(streetAndHouseNumberField, 10, 10, Short.MAX_VALUE))
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-								.addComponent(addPhone, 10, 10, Short.MAX_VALUE).addComponent(phoneField, 10, 10, Short.MAX_VALUE).addComponent(addSex, 10, 10, Short.MAX_VALUE).addComponent(sexCB, 10, 10, Short.MAX_VALUE))
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-								.addComponent(addLicenceNumber, 10, 10, Short.MAX_VALUE).addComponent(licenceNumberField, 10, 10, Short.MAX_VALUE).addComponent(addSpecialty, 10, 10, Short.MAX_VALUE).addComponent(specialtyCB, 10, 10, Short.MAX_VALUE)))
-				.addGroup(doctorPanelLayout.createSequentialGroup()
-						.addComponent(addDateOfEmployment, 10, 10, Short.MAX_VALUE).addGap(10,10,10).addComponent(dateOfEmploymentField, 10, 10, Short.MAX_VALUE).addGap(247, 247, Short.MAX_VALUE))
-				.addGap(30, 30, Short.MAX_VALUE)
-				.addGroup(doctorPanelLayout.createSequentialGroup()
-						.addComponent(wrongData).addGap(100, 100, Short.MAX_VALUE).addComponent(addDoctor).addComponent(mainMenuButton))
-				);
-		
-		doctorPanelLayout.setVerticalGroup(doctorPanelLayout.createSequentialGroup()
-				.addComponent(addDoctorTitle)
-				.addGap(30, 30, Short.MAX_VALUE)
-				.addGroup(doctorPanelLayout.createParallelGroup()
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-							.addComponent(addName, 20, 20, Short.MAX_VALUE).addComponent(addPersonalNumber, 20, 20, Short.MAX_VALUE).addComponent(addCountryOfBirth, 20, 20, Short.MAX_VALUE).addComponent(addPostalCode, 20, 20, Short.MAX_VALUE).addComponent(addPhone, 20, 20, Short.MAX_VALUE).addComponent(addLicenceNumber, 20, 20, Short.MAX_VALUE))
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-							.addComponent(nameField, 20, 20, Short.MAX_VALUE).addComponent(personalNumberField, 20, 20, Short.MAX_VALUE).addComponent(countryOfBirthCB, 20, 20, Short.MAX_VALUE).addComponent(postalCodeField, 20, 20, Short.MAX_VALUE).addComponent(phoneField, 20, 20, Short.MAX_VALUE).addComponent(licenceNumberField, 20, 20, Short.MAX_VALUE))
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-								.addComponent(addSurname, 20, 20, Short.MAX_VALUE).addComponent(addDateOfBirth, 20, 20, Short.MAX_VALUE).addComponent(addCity, 20, 20, Short.MAX_VALUE).addComponent(addStreetAndHouseNumber, 20, 20, Short.MAX_VALUE).addComponent(addSex, 20, 20, Short.MAX_VALUE).addComponent(addSpecialty, 20, 20, Short.MAX_VALUE))
-						.addGroup(doctorPanelLayout.createSequentialGroup()
-								.addComponent(surnameField, 20, 20, Short.MAX_VALUE).addComponent(dateOfBirthField, 20, 20, Short.MAX_VALUE).addComponent(cityField, 20, 20, Short.MAX_VALUE).addComponent(streetAndHouseNumberField, 20, 20, Short.MAX_VALUE).addComponent(sexCB, 20, 20, Short.MAX_VALUE).addComponent(specialtyCB, 20, 20, Short.MAX_VALUE)))
-				.addGroup(doctorPanelLayout.createParallelGroup()
-						.addComponent(addDateOfEmployment, 20, 20, 23).addComponent(dateOfEmploymentField, 10, 10, 23))
-				.addGap(30, 30, Short.MAX_VALUE)
-				.addGroup(doctorPanelLayout.createParallelGroup()
-						.addComponent(wrongData).addComponent(addDoctor).addComponent(mainMenuButton))
-				);
 		//Add doctor button in add doctor panel
 		addDoctor.addActionListener(new ActionListener() {
 
@@ -178,6 +258,13 @@ public class NeurologyClinic extends Clinic{
 					}
 			}
 		});
+		
+		// Patient Panel
+		addPatientTitle = new JLabel("Add doctor");
+		addPatientTitle.setFont(new Font("Arial", Font.BOLD, 37));
+		addDateOfRegistration = new JLabel("Date of registration (yyyy-mm-dd):");
+		dateOfRegistrationField = new JTextField();
+		
 		
 		clinic.closeConnection();
 		frame.setBounds(300, 200, 930, 400);
--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,11 +1,19 @@
 package model;
 
 public class Patient extends Person {
-
+	private String dateOfRegistration;
+	
+	public String getDateOfRegistration() {
+		return dateOfRegistration;
+	}
+	public void setDateOfRegistration(String dateOfRegistration) {
+		this.dateOfRegistration = dateOfRegistration;
+	}
+	
 	public Patient(String name, String surname, String personalNumber, String dateOfBirth, String countryOfBirth, String city,
-			String postalCode, String streetAndHouseNumber, String phone, String sex) {
+			String postalCode, String streetAndHouseNumber, String phone, String sex, String dateOfRegistration) {
 		super(name, surname, personalNumber, dateOfBirth, countryOfBirth, city, postalCode, streetAndHouseNumber, phone, sex);
-		// TODO Auto-generated constructor stub
+		this.dateOfRegistration = dateOfRegistration;
 	}
 
 }
